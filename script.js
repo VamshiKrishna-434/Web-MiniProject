2d48adcc57d5cb5b583658d7be277e468eee933c
VamshiKrishna-434 <115861228+VamshiKrishna-434@users.noreply.github.com>
2023-03-11 18:05:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+
+const options = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': '5e7a3b0268msh23be5d962a01b74p124f22jsn6f06a8d6670e',
+        'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+    }
+};
+const getWeather = (city) => {
+    cityName.innerHTML=city
+    fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city='+city, options)
+    .then(response => response.json())
+    .then((response) => {
+
+        console.log(response)
+        cloud_pct.innerHTML = response.cloud_pct
+        temp.innerHTML = response.temp
+        feels_like.innerHTML = response.feels_like
+        humidity.innerHTML = response.humidity
+        min_temp.innerHTML = response.min_temp
+        max_temp.innerHTML = response.max_temp
+        wind_speed.innerHTML = response.wind_speed
+        sunrise.innerHTML = response.sunrise
+        sunset.innerHTML = response.sunset
+    })
+    .catch(err => console.error(err));
+}
+submit.addEventListener("click",(e)=>{
+    e.preventDefault()
+    getWeather(city.value)
+})
+getWeather("Bengaluru")
